f5fec23b7b58c0dcaef6d78dcf8b8c6f86ef5bf7
riswandii-lop <Litastore41@gmail.com>
2025-03-12 23:59:39 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -114,7 +114,7 @@ function getAllConfig(request, hostName, proxyList, page = 0) {
 
     // Build HTML
     const document = new Document(request);
-    document.setTitle("Welcome to <span class='text-blue-500 font-semibold'>JessVpntunnel</span>");
+    document.setTitle("hayy<span class='text-blue-500 font-semibold'>Selamat datang! Terima kasih telah mengunjungi JessVpntunnel</span>");
     document.addInfo(`Total: ${proxyList.length}`);
     document.addInfo(`Page: ${page}/${Math.floor(proxyList.length / PROXY_PER_PAGE)}`);
 
